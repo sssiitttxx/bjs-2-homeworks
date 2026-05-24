ee7becb9df090fd5235616356ecebb5b7cc4b349
Andrey <morgenshtern8888@gmail.com>
2026-05-24 16:33:36 +0300

    Обновлено решение задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,15 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   const d = b ** 2 - 4 * a * c;
-  if ( d < 0 ) {
+  if (d < 0) {
     return [];
-  } else if ( d === 0) {
-    const x = -b / (2 * a )
+  } else if (d === 0) {
+    const x = -b / (2 * a);
     return [x];
   } else {
-    const x1 = (-b + Math.sqrt(d) )/(2*a);
-    const x2 = (-b - Math.sqrt(d) )/(2*a);
-    return [x1, x2]
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
+    return [x1, x2];
   }
 }
 
@@ -17,13 +17,13 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let P = percent / 100 / 12;
 
   let S = amount - contribution;
-  if ( S <= 0) {
-  return 0;
+  if (S <= 0) {
+    return 0;
   }
 
-  let degree = Math.pow((1 + P), countMonths);
-  let payment = S * (P + (P / (degree - 1)));
-  
+  let degree = Math.pow(1 + P, countMonths);
+  let payment = S * (P + P / (degree - 1));
+
   let totalAmount = payment * countMonths;
   return Number(totalAmount.toFixed(2));
 }
